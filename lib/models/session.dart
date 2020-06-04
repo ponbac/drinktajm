9fb3f2a1cd1c95be4073b21f37ecd13ee6677035
ponbac <ponbac@student.chalmers.se>
2020-06-04 18:30:04 +0200

    now parsing [sips] and [player]

--- lib/models/session.dart
+++ lib/models/session.dart
@@ -1,9 +1,11 @@
 import 'package:drinkinggame/models/player.dart';
 import 'package:drinkinggame/models/question.dart';
-import 'package:drinkinggame/resources/question_provider.dart';
-import 'package:flutter/material.dart';
+
+import 'dart:math';
 
 class Session {
+  static final Random rng = new Random();
+
   Question currentQuestion;
   List<Question> _questionList;
   List<Question> _usedQuestions = new List<Question>();
@@ -27,7 +29,9 @@ class Session {
 
   void nextQuestion() {
     if (_questionList.length == 0) {
-      _questionList = []..addAll(_usedQuestions)..shuffle();
+      _questionList = []
+        ..addAll(_usedQuestions)
+        ..shuffle();
       _usedQuestions.clear();
       print('Reset questions!');
     }
@@ -47,6 +51,35 @@ class Session {
     int sips = question.sips;
     bool guaranteed = question.guaranteed;
 
+    if (questionText.contains('[sips]')) {
+      questionText = questionText.replaceAll('[sips]', sips.toString());
+    }
+
+    if (questionText.contains('[player1]')) {
+      int nmbrOfPlayers = playerList.length;
+      List<Player> playerListCopy = []..addAll(playerList);
+
+      Player randomPlayer1 = playerListCopy.elementAt(rng.nextInt(nmbrOfPlayers));
+      playerListCopy.remove(randomPlayer1);
+      questionText = questionText.replaceAll('[player1]', randomPlayer1.name);
+
+      if (questionText.contains('[player2]') && nmbrOfPlayers >= 2) {
+        Player randomPlayer2 = playerListCopy.elementAt(rng.nextInt(nmbrOfPlayers - 1));
+        playerListCopy.remove(randomPlayer2);
+        questionText = questionText.replaceAll('[player2]', randomPlayer2.name);
+      }
+
+      if (questionText.contains('[player3]') && nmbrOfPlayers >= 3) {
+        Player randomPlayer3 = playerListCopy.elementAt(rng.nextInt(nmbrOfPlayers - 2));
+        playerListCopy.remove(randomPlayer3);
+        questionText = questionText.replaceAll('[player3]', randomPlayer3.name);
+      }
+
+      if (guaranteed) {
+        randomPlayer1.addSips(sips);
+      }
+    }
+
     if (questionText.contains('[everyone]')) {
       if (questionText.startsWith('[')) {
         questionText = questionText.replaceAll('[everyone]', 'Alla');
@@ -57,7 +90,7 @@ class Session {
       if (guaranteed) {
         playerList.forEach((player) {
           player.addSips(sips);
-          print('${player.name} = ${player.sips} sips!');
+          //print('${player.name} = ${player.sips} sips!');
         });
       }
     }
