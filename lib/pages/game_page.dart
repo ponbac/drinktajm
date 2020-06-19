d2f8aa4871aee195f6fb8d2fd99526c93b807913
ponbac <ponbac@student.chalmers.se>
2020-06-19 11:25:25 +0200

    added wave background to game page

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -7,6 +7,7 @@ import 'package:drinkinggame/widgets/action_button.dart';
 import 'package:drinkinggame/widgets/header_text.dart';
 import 'package:drinkinggame/widgets/question_card.dart';
 import 'package:drinkinggame/constants.dart';
+import 'package:drinkinggame/widgets/wave_background.dart';
 
 import 'package:flutter/material.dart';
 
@@ -35,78 +36,83 @@ class _GamePageState extends State<GamePage> {
     Session session = widget.session;
 
     return Scaffold(
-      body: FutureBuilder<List<Question>>(
-        future: widget.loadQuestions,
-        builder:
-            (BuildContext context, AsyncSnapshot<List<Question>> snapshot) {
-          List<Widget> children;
+      body: Stack(
+        children: <Widget>[
+          WaveBackground(),
+          FutureBuilder<List<Question>>(
+            future: widget.loadQuestions,
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Question>> snapshot) {
+              List<Widget> children;
 
-          if (snapshot.hasData) {
-            if (session.questionListLoaded == false) {
-              session.questionList = snapshot.data;
-            }
+              if (snapshot.hasData) {
+                if (session.questionListLoaded == false) {
+                  session.questionList = snapshot.data;
+                }
 
-            Question currentQuestion = session.currentQuestion;
-            _currentCategory = currentQuestion.category;
-            //print('built widget');
+                Question currentQuestion = session.currentQuestion;
+                _currentCategory = currentQuestion.category;
+                //print('built widget');
 
-            children = <Widget>[
-              QuestionCard(currentQuestion, session),
-              Container(
-                height: 48,
-                child: ActionButton(
-                  buttonTitle: 'Nästa',
-                  onPress: () {
-                    setState(() {
-                      session.nextQuestion();
-                      session.displayingAnswer = false;
-                    });
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Center(
-                  child: Ink(
-                decoration: const ShapeDecoration(
-                  color: Color(0xFF1089ff),
-                  shape: CircleBorder(),
-                ),
-                child: IconButton(
-                  icon: Icon(Icons.supervisor_account),
-                  color: Colors.white,
-                  onPressed: () => _viewScore(context, session.playerList),
-                ),
-              )),
-            ];
-          } else {
-            children = <Widget>[
-              SizedBox(
-                child: CircularProgressIndicator(),
-                width: 60,
-                height: 60,
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 16),
-                child: Text(
-                  'Laddar frågor...',
-                  style: TextStyle(fontSize: 26, color: Colors.amber),
-                ),
-              )
-            ];
-          }
+                children = <Widget>[
+                  QuestionCard(currentQuestion, session),
+                  Container(
+                    height: 48,
+                    child: ActionButton(
+                      buttonTitle: 'Nästa',
+                      onPress: () {
+                        setState(() {
+                          session.nextQuestion();
+                          session.displayingAnswer = false;
+                        });
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                      child: Ink(
+                    decoration: const ShapeDecoration(
+                      color: Color(0xFF1089ff),
+                      shape: CircleBorder(),
+                    ),
+                    child: IconButton(
+                      icon: Icon(Icons.supervisor_account),
+                      color: Colors.white,
+                      onPressed: () => _viewScore(context, session.playerList),
+                    ),
+                  )),
+                ];
+              } else {
+                children = <Widget>[
+                  SizedBox(
+                    child: CircularProgressIndicator(),
+                    width: 60,
+                    height: 60,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16),
+                    child: Text(
+                      'Laddar frågor...',
+                      style: TextStyle(fontSize: 26, color: Colors.amber),
+                    ),
+                  )
+                ];
+              }
 
-          return Container(
-            decoration: _categoryBackgroundColor(),
-            child: Center(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: children),
-            ),
-          );
-        },
+              return Container(
+                //decoration: _categoryBackgroundColor(),
+                child: Center(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: children),
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widgets/wave_background.dart
@@ -0,0 +1,38 @@
+import 'package:drinkinggame/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:wave/wave.dart';
+import 'package:wave/config.dart';
+
+class WaveBackground extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WaveWidget(
+      config: CustomConfig(
+        gradients: [
+          [
+            Color.fromRGBO(72, 74, 126, 1),
+            Color.fromRGBO(125, 170, 206, 1),
+            Color.fromRGBO(184, 189, 245, 0.7)
+          ],
+          [
+            Color.fromRGBO(72, 74, 126, 1),
+            Color.fromRGBO(125, 170, 206, 1),
+            Color.fromRGBO(172, 182, 219, 0.7)
+          ],
+          [
+            Colors.black54,
+            Colors.black45,
+            Colors.black38
+          ],
+        ],
+        durations: [19440, 10800, 6000],
+        heightPercentages: [0.03, 0.01, 0.02],
+        gradientBegin: Alignment.bottomCenter,
+        gradientEnd: Alignment.topCenter,
+      ),
+      size: Size(double.infinity, double.infinity),
+      waveAmplitude: 25,
+      backgroundColor: kBackgroundColor,
+    );
+  }
+}
