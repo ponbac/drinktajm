e806415e1d34808dec6211567f859215a8032646
ponbac <ponbac@student.chalmers.se>
2020-06-28 17:16:00 +0200

    tried adding neumorphism button

--- lib/pages/one_vs_one_page.dart
+++ lib/pages/one_vs_one_page.dart
@@ -1,3 +1,4 @@
+import 'package:drinkinggame/widgets/header_text.dart';
 import 'package:flutter/material.dart';
 
 class OneVsOne extends StatelessWidget {
@@ -5,12 +6,39 @@ class OneVsOne extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'UNDER CONSTRUCTION',
-          textAlign: TextAlign.center,
-        ),
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.all(Radius.circular(40)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey[500],
+                        offset: Offset(8.0, 8.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 1.0),
+                    BoxShadow(
+                        color: Colors.white,
+                        offset: Offset(-8.0, -8.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 1.0)
+                  ]),
+              child: Center(
+                  child: Icon(
+                Icons.local_drink,
+                size: 80,
+                color: Colors.black87,
+              )),
+            ),
+          ),
+        ],
       ),
     );
   }
